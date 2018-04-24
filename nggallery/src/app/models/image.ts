ddabbbf750b03e10502691ba7d83f90d8d0f6210
unknown <gustavo0326@gmail.com>
2018-04-24 00:49:45 -0500

    separando la logica y el componente gallery en lista e imagen y agregando un modelo

---
+++ nggallery/src/app/models/image.ts
@@ -0,0 +1,7 @@
+export class Image {
+    constructor(public id:string, public title:string,
+    public descripcion:string, public thumbnail:string,
+    public imageLink:string){
+        
+    }
+}
